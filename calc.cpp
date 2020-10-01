a923e4f94f1f35148d5884326fcd79faa45054d6
ROHITH C DINESAN <56485934+rohithmaddz@users.noreply.github.com>
2020-10-01 23:30:49 +0530

    calc

---
+++ calc.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+
+	double num1, num2, result;
+	int value;
+
+	cout << "Select one of the operations" << endl;
+	cout << "1. Addition \t\t 2. Subtraction \t\t 3. Multiplication \t\t 4. Division" << endl;
+	cin >> value;
+
+	cout << "Enter the two numbers" << endl;
+	cout << "num1: ";
+	cin >> num1;
+	cout << "num2: ";
+	cin >> num2;
+
+	switch(value)
+	{
+		case 1:
+			result = num1 + num2;
+			cout << "Result "<< result << endl;
+		  	break;
+
+		case 2:
+			result = num1 - num2;
+			cout << "Result "<< result << endl;
+		  	break;
+
+		case 3:
+			result = num1 * num2;
+			cout << "Result "<< result << endl;
+		  	break;
+
+		case 4:
+			result = num1 / num2;
+			cout << "Result "<< result << endl;
+		  	break;
+
+		default:
+			cout << "Error!! input" << endl;
+	}
+	  
+	 
+	system("pause");
+	return 0;
+}
